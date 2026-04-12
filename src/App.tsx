ccf42eb0e34c65079faf42233192a755c8e104ee
alaminsahed <alaminsahed101@gmail.com>
2026-04-12 17:05:45 +0600

    refactor: restructure routing in App component and add Loading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,38 @@
 import { Suspense, lazy } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from 'react-router-dom'
 
+import { PUBLIC } from '@/constant/app-routes'
 import { publicRoutes } from '@/routes/public'
+import Loading from '@/components/loading'
 
 const NotFound = lazy(() => import('@/pages/not-found'))
+
+const AppRoutes = () => {
+  const location = useLocation()
+
+  return (
+    <Suspense key={location.pathname} fallback={<Loading />}>
+      <Routes>
+        <Route index element={<Navigate to={PUBLIC.TODOS} replace />} />
+        {publicRoutes.map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
+  )
+}
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          {publicRoutes.map(({ path, element }) => (
-            <Route key={path} path={path} element={element} />
-          ))}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
+      <AppRoutes />
     </BrowserRouter>
   )
 }
